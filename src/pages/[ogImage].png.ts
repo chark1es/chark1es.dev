d3af1b9b65a98bf31c4176aea70c5679336855ef
chark1es <charles15851@gmail.com>
2024-12-08 23:23:23 -0800

    migration to v5 and cleanup unused components

--- src/pages/[ogImage].png.ts
+++ src/pages/[ogImage].png.ts
@@ -5,13 +5,13 @@ import type { APIRoute } from "astro";
 export const GET: APIRoute = async ({ params }) => {
     const ogImage = await generateOgImage(params.ogImage);
     return new Response(JSON.stringify({ body: ogImage }), {
-        headers: { "Content-Type": "application/json" }
+        headers: { "Content-Type": "application/json" },
     });
 };
 
 const postImportResult = await getCollection(
     "blogs",
-    ({ data }) => !data.draft
+    ({ data }) => !data.draft,
 );
 const posts = Object.values(postImportResult);
 
@@ -19,6 +19,6 @@ export function getStaticPaths() {
     return posts
         .filter(({ data }) => !data.heroImage)
         .map(({ data }) => ({
-            params: { ogImage: data.title }
+            params: { ogImage: data.title },
         }));
 }
